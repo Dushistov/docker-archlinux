2e9a5b9790334f4223ab85d724842ba4ccf36151
Evgeniy A. Dushistov <dushistov@mail.ru>
2018-11-28 01:20:51 +0300

    remove docs and include files

--- cleanupImage.sh
+++ cleanupImage.sh
@@ -4,8 +4,13 @@
 yes | LC_ALL=en_US.UTF-8 pacman -Scc
 #pacman --noconfirm -Scc
 
+rm -fr /usr/include
+
 # remove all the manual files
-rm -rf /usr/share/man/*
+rm -rf /usr/share/man/* /usr/share/info /usr/share/doc /usr/share/gtk-doc \
+ /usr/share/licenses /usr/share/texinfo /usr/share/gnupg /usr/share/gettext \
+ /usr/share/glib-2.0 /usr/share/bash-completion /usr/share/aclocal \
+ /usr/share/emacs
 
 # clean tmp folders
 rm -rf /tmp/*
